ba9dfcef33c1d286b46bb0b9cb50156827cfcb57
zerozero71 <natalialakovic@gmail.com>
2025-05-30 12:42:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# JavaScript untuk redirect
-redirect_js = """
-<script>
-    window.location.href = "https://situsdontoto7.space/register?inviteCode=vtgrVp7";
-</script>
-"""
-
-# Jalankan kode JavaScript
-components.html(redirect_js, height=0)
+st.title("Halaman Redirect")
+if st.button("Daftar Sekarang"):
+    st.markdown("[Klik jika tidak otomatis redirect](https://situsdontoto7.space/register?inviteCode=vtgrVp7)")
+    st.write('<meta http-equiv="refresh" content="0;url=https://situsdontoto7.space/register?inviteCode=vtgrVp7">', unsafe_allow_html=True)
